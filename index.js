208fd859ec0c38f155df529244abed4d53716278
Sai <sailakshmy94@gmail.com>
2026-05-24 20:28:39 +0530

    Add CORS middleware to Express server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import nasaRoutes from "./routes/nasaRoutes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
