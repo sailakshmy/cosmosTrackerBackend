b627c31b4a49d6731bdf41f571278c79ba2ab615
Sai <sailakshmy94@gmail.com>
2026-05-29 07:55:01 +0300

    Add APOD fallback for unavailable dates

--- routes/nasaRoutes.js
+++ routes/nasaRoutes.js
@@ -6,10 +6,22 @@ const nasaRouter = express.Router();
 nasaRouter.get("/", async (req, res, next) => {
   const { date } = req.query;
   console.log("Req", req.query);
-  const apodPictureOfTheDay = await fetch(
+  let apodPictureOfTheDay = await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=${date}`,
   );
-  const apodData = await apodPictureOfTheDay.json();
+  let apodData = await apodPictureOfTheDay.json();
+  if (apodData?.msg?.includes("No data available for date")) {
+    console.log("No available data block");
+    const previousDate = new Date();
+    previousDate?.setDate(previousDate?.getDate() - 1);
+    const priorDate = previousDate?.toISOString()?.split("T")?.[0];
+    apodPictureOfTheDay = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=${priorDate}`,
+    );
+    apodData = await apodPictureOfTheDay.json();
+    apodData = { ...apodData, updatedDate: priorDate };
+  }
+  console.log("Apod", apodData);
   res.json({ message: "NASA get route is working", data: { ...apodData } });
 });
 
