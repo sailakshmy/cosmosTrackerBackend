78ff7381eefb20e0780207b6f6da6fdea73ffcff
Sai <sailakshmy94@gmail.com>
2026-07-09 23:45:55 +0400

    Use configured TLE API with error handling

--- services/tleService.js
+++ services/tleService.js
@@ -1,5 +1,21 @@
 export const getTleList = async () => {
-  const tleRes = await fetch(`http://tle.ivanstanojevic.me/api/tle`);
-  const tleData = await tleRes?.json();
-  return tleData;
+  let tleListData;
+  let message;
+  try {
+    const tleRes = await fetch(`${process.env.TLE_API}`);
+    if (tleRes.ok) {
+      const tleData = await tleRes?.json();
+      message = "NASA TLE List Route is working";
+      tleListData = { ...tleData };
+    } else {
+      throw new Error(`NASA TLE List API returned ${tleRes.status}`);
+    }
+  } catch (e) {
+    console.error("Error while fetching TLE List from NASA");
+    throw e;
+  }
+  return {
+    tleListData,
+    message,
+  };
 };
